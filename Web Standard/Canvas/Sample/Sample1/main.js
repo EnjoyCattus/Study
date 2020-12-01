706a869a8cf045b9e5f389130c619bf051a48350
EnjoyCattus <enjoycattus1217@gmail.com>
2020-12-02 01:40:03 +0900

    Draw Rectangle

--- Web Standard/Canvas/Sample/Sample1/main.js
+++ Web Standard/Canvas/Sample/Sample1/main.js
@@ -1,1 +1,14 @@
 console.log('Create Sample1');
+
+let canvas = document.createElement('canvas');
+let canvasWidth = canvas.width;
+let canvasHeight = canvas.height;
+
+document.body.appendChild(canvas);
+
+console.log('Canvas Width : ' + canvasWidth);
+console.log('Canvas Height : ' + canvasHeight);
+
+let context = canvas.getContext('2d');
+context.fillStyle = 'blue';
+context.fillRect(10, 10, 150, 150);
