1bf5c409f4ce8972d9e41789f2d7baddfe33c40b
EnjoyCattus <enjoycattus1217@gmail.com>
2020-12-02 23:32:52 +0900

    Resize Canvas Size

--- Web Standard/Canvas/Sample/Sample1/main.js
+++ Web Standard/Canvas/Sample/Sample1/main.js
@@ -1,11 +1,12 @@
 console.log('Create Sample1');
 
 let canvas = document.createElement('canvas');
-canvas.width = 500;
-canvas.height = 500;
+canvas.width = window.innerWidth - 10;
+canvas.height = window.innerHeight - 10;
 document.body.appendChild(canvas);
 
 let context = canvas.getContext('2d');
+
 context.fillStyle = 'blue';
 context.fillRect(10, 10, 400, 400);
 
