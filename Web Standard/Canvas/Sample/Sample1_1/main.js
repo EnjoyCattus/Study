d45e9a36de2e15e053a3fac353ac4cd72c7acd6c
EnjoyCattus <enjoycattus1217@gmail.com>
2020-12-02 23:34:10 +0900

    Create Sample1_1

---
+++ Web Standard/Canvas/Sample/Sample1_1/main.js
@@ -0,0 +1,17 @@
+console.log('Create Sample1');
+
+let canvas = document.createElement('canvas');
+canvas.width = window.innerWidth - 10;
+canvas.height = window.innerHeight - 10;
+document.body.appendChild(canvas);
+
+
+let canvasWidth = canvas.width;
+let canvasHeight = canvas.height;
+console.log('Canvas Width : ' + canvasWidth);
+console.log('Canvas Height : ' + canvasHeight);
+
+
+
+
+
